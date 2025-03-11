0b7a532bc04c1667bbcccfade197367ac7c5c50c
pedromicarmo <pecarmo@deloitte.pt>
2025-03-11 19:15:43 +0000

    add external dns deployment

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -42,3 +42,77 @@ resource "azurerm_role_assignment" "aks_acr_reader" {
   role_definition_name = "Reader"
   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
+
+# data "azurerm_virtual_network" "aks_vnet" {
+#   name                = "MC_${azurerm_kubernetes_cluster.aks.resource_group_name}_${azurerm_kubernetes_cluster.aks.name}_${azurerm_kubernetes_cluster.aks.location}"
+#   resource_group_name = azurerm_kubernetes_cluster.aks.node_resource_group
+# }
+
+# resource "azurerm_subnet" "ingress_appgw_subnet" {
+#   name                 = "ingress-appgw-subnet"
+#   resource_group_name  = data.azurerm_virtual_network.aks_vnet.resource_group_name
+#   virtual_network_name = data.azurerm_virtual_network.aks_vnet.name
+#   address_prefixes     = ["10.240.2.0/24"]
+# }
+#
+# resource "azurerm_public_ip" "appgw_public_ip" {
+#   name                = "appgw-public-ip"
+#   resource_group_name = var.resource_group_name
+#   location            = var.location
+#   allocation_method   = "Static"
+# }
+#
+# resource "azurerm_application_gateway" "ingress_appgw" {
+#   name                = "ingress-appgw"
+#   resource_group_name = var.resource_group_name
+#   location            = var.location
+#
+#   sku {
+#     name     = "Standard_v2"
+#     tier     = "Standard_v2"
+#     capacity = 2
+#   }
+#
+#   gateway_ip_configuration {
+#     name      = "appgw-ipconfig"
+#     subnet_id = azurerm_subnet.ingress_appgw_subnet.id
+#   }
+#
+#   frontend_port {
+#     name = "frontend-port"
+#     port = 80
+#   }
+#
+#   frontend_ip_configuration {
+#     name                 = "frontend-ip"
+#     public_ip_address_id = azurerm_public_ip.appgw_public_ip.id
+#   }
+#
+#   request_routing_rule {
+#     name                       = "ingress-rule"
+#     rule_type                  = "Basic"
+#     http_listener_name         = "listener"
+#     backend_address_pool_name  = "pool"
+#     backend_http_settings_name = "http-settings"
+#   }
+#
+#   http_listener {
+#     name                           = "listener"
+#     frontend_ip_configuration_name = "frontend-ip"
+#     frontend_port_name             = "frontend-port"
+#     protocol                       = "Http"
+#   }
+#
+#   backend_address_pool {
+#     name = "pool"
+#   }
+#
+#   backend_http_settings {
+#     name                  = "http-settings"
+#     cookie_based_affinity = "Disabled"
+#     port                  = 80
+#     protocol              = "Http"
+#     request_timeout       = 20
+#     probe_name            = "probe"
+#   }
+# }
